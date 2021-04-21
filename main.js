eb31de213d1be1afb6bf3a71a13782205c1e9c3c
June-yu09 <yuzatee@gmail.com>
2021-04-21 23:36:52 +0200

    chaining fetch func 2

--- main.js
+++ main.js
@@ -15,28 +15,25 @@ if (document.title == "Home"){
 
 
 function fetchData(){
-    fetch("https://www.rijksmuseum.nl/api/en/collection?key=gDhkonP6&toppieces=True&ps=25&type=painting")
-    .then( response => response.json() )
-    
-    .then( data => {        
-        data.artObjects.forEach((d)=>{
-            const oN = d.objectNumber;
-            console.log(fetch(`https://www.rijksmuseum.nl/api/en/collection/${oN}?key=gDhkonP6`))
-            return fetch(`https://www.rijksmuseum.nl/api/en/collection/${oN}?key=gDhkonP6`).then( response => response.json() )
-            .then((a)=>{
-                loading("")
-                displayData(a);
-            })
-            .catch((err)=>{
-                console.log(err);
-            })
+    fetch("https://www.rijksmuseum.nl/api/en/collection?key=gDhkonP6&toppieces=True&ps=30")
+    .then( response => response.json())
+    .then( data => {
+        return data;
     })
-    
-})
-.catch((err)=>{
-    console.log(err);
-})
+    .then(async data => {
+        await Promise.all(data.artObjects.map(d => {
+            return fetch(`https://www.rijksmuseum.nl/api/en/collection/${d.objectNumber}?key=gDhkonP6`)
+                .then(response => response.json())
+                .then(data => {
+                    loading("")
+                    displayData(data);
+                })
+                .catch(err=> console.log(err))
+        }))
+    })
+    .catch(err=> console.log(err))
 }
+
 
 
 function loading(str){
@@ -48,27 +45,113 @@ function displayData(data){
     const d = data.artObject
     const newRow = document.createElement("tr");
     tb.appendChild(newRow);
+
     const newType = document.createElement("td");
     newType.innerHTML = d.objectTypes;
     tb.appendChild(newType);
+
     const newArtist = document.createElement("td");
     newArtist.innerHTML = d.principalMakers[0].name;
     tb.appendChild(newArtist);
+
     const newTitle = document.createElement("td");
     newTitle.innerHTML = d.title;
     tb.appendChild(newTitle);
+
     const newPeriod = document.createElement("td");
     newPeriod.innerHTML = d.dating.period;
     tb.appendChild(newPeriod);
+
     const myImage = document.createElement("IMG");
     myImage.setAttribute("src", d.webImage.url);
     myImage.setAttribute("width", "400px")
     myImage.setAttribute("height","400px")
     tb.appendChild(myImage);
+
+
 }
 
 
 if (document.title=="Search"){
     fetchData();
     loading("LOADING...");
+} else if (document.title =="Our top pieces"){
+    fetchData2();
+    loading("Loading...");
 }
+
+// const addEvents = () => {
+//     let checkboxes = Array.from(document.querySelectorAll('input[type=checkbox]'));
+//     console.log(checkboxes);
+//     checkboxes.forEach((checkbox)=>{
+//         checkbox.addEventListener("change",()=>{
+//             filterData();
+//         })
+//     })
+// }
+
+// const filterData = () => {
+//     let checkBoxes = Array.from(document.querySelectorAll('input[type=checkbox]:checked')).map((checkbox)=>{
+//         return checkbox.value;
+//     })
+//     console.log("checkBoxes",checkBoxes);
+//     if (checkBoxes.length !== 0){
+        
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+function fetchData2(){
+    fetch("https://www.rijksmuseum.nl/api/en/collection?key=gDhkonP6&toppieces=True&ps=50&type=painting")
+    .then( response => response.json() )
+    
+    .then( data => {
+        return data;
+    })
+    .then(async data => {
+        await Promise.all(data.artObjects.map(d => {
+            return fetch(`https://www.rijksmuseum.nl/api/en/collection/${d.objectNumber}?key=gDhkonP6`)
+                .then(response => response.json())
+                .then(data => {
+                    loading("")
+                    displayData(data);
+
+                })
+                .catch(err=> console.log(err))
+        }))
+    })
+    .catch(err=> console.log(err))
+}
+
+
+
+// function fetchData(){
+//     fetch("https://www.rijksmuseum.nl/api/en/collection?key=gDhkonP6&toppieces=True&ps=20")
+//     .then( response => response.json() )
+    
+//     .then( data => {        
+//         data.artObjects.forEach((d)=>{
+//             const oN = d.objectNumber;
+//             console.log(fetch(`https://www.rijksmuseum.nl/api/en/collection/${oN}?key=gDhkonP6`))
+//             return fetch(`https://www.rijksmuseum.nl/api/en/collection/${oN}?key=gDhkonP6`).then( response => response.json() )
+//             .then((a)=>{
+//                 loading("")
+//                 displayData(a);
+//             })
+//             .catch((err)=>{
+//                 console.log(err);
+//             })
+//         })
+//     })
+//     .catch((err)=>{
+//         console.log(err);
+//     })
+// }
